8a27172188bf9acad9b79f4d594d7753600972b2
Alexandr Gerasimov <sashagerasimov55@gmail.com>
2024-07-31 19:14:56 +0300

    Переписал js и сделал классовый объект

--- index.js
+++ index.js
@@ -1,47 +1,62 @@
-const progressCircle = document.querySelector(".progress__circle");
-const circle = document.querySelector(".circle");
-const frontCircle = progressCircle.querySelector(".circle__front");
-const percentInput = document.getElementById("valueInput");
-const animateInput = document.getElementById("animateInput");
-const hideInput = document.getElementById("hideInput");
+class ProgressCircle {
+  constructor(container) {
+    this.container = document.querySelector(`.${container}`);
+    this.progressCircle = this.container.querySelector(".progress__circle");
+    this.circle = this.container.querySelector(".circle");
+    this.frontCircle = this.container.querySelector(".circle__front");
+    this.percentInput = document.getElementById("valueInput");
+    this.animateInput = document.getElementById("animateInput");
+    this.hideInput = document.getElementById("hideInput");
 
-// Слушатель на то, чтобы не вводили числа меньше 0 и больше 100
-percentInput.addEventListener("input", function () {
-  let value = parseInt(this.value, 10);
-  if (this.value === "") {
-    value = 0;
+    this.isAnimating = false;
+    this.isHidden = false;
+
+    this.percentInput.addEventListener("input", () =>
+      this.setValue(this.percentInput.value)
+    );
+    this.animateInput.addEventListener("change", () =>
+      this.toggleAnimate(this.animateInput.checked)
+    );
+    this.hideInput.addEventListener("change", () =>
+      this.toggleHide(this.hideInput.checked)
+    );
+
+    this.updateProgress(0);
   }
-  if (value > 100) {
-    this.value = 100;
-    value = 100;
-  } else if (value < 0) {
-    this.value = 0;
-    value = 0;
+
+  setValue(value) {
+    value = parseInt(value, 10);
+    if (this.percentInput.value === "") {
+      value = 0;
+    }
+    if (value > 100) {
+      this.percentInput.value = 100;
+      value = 100;
+    } else if (value < 0) {
+      this.percentInput.value = 0;
+      value = 0;
+    }
+
+    this.updateProgress(value);
   }
 
-  updateProgress(value);
-});
+  updateProgress(value) {
+    const radius = this.frontCircle.attributes.r.value;
+    const circumference = 2 * Math.PI * radius;
+    const offset = circumference - (value / 100) * circumference;
+    this.frontCircle.style.strokeDasharray = `${circumference} ${circumference}`;
+    this.frontCircle.style.strokeDashoffset = offset;
+  }
 
-// Обновление дуги прогресса
-function updateProgress(value) {
-  const offset = 566 - (566 * value) / 100;
-  frontCircle.style.strokeDashoffset = offset;
+  toggleAnimate(isAnimating) {
+    isAnimating
+      ? this.circle.classList.add("rotating")
+      : this.circle.classList.remove("rotating");
+  }
+
+  toggleHide(isHidden) {
+    this.progressCircle.style.opacity = isHidden ? "0" : "1";
+  }
 }
 
-// Слушатель на скрытие/показ круга-прогресса
-hideInput.addEventListener("change", (e) => {
-  if (e.target.checked) {
-    progressCircle.style.opacity = "0";
-  } else {
-    progressCircle.style.opacity = "1";
-  }
-});
-
-// Слушатель на анимацию
-animateInput.addEventListener("change", (e) => {
-  if (e.target.checked) {
-    circle.classList.add("rotating");
-  } else {
-    circle.classList.remove("rotating");
-  }
-});
+const progressBlock = new ProgressCircle("progress");
